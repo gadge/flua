68a4875776d4e2fef1050da8807acd05784887c9
alkashi <hoyeungw@outlook.com>
2020-03-23 16:05:50 +0800

    update dependencies

---
+++ output/PlantToSku.js
@@ -0,0 +1,11 @@
+export const PlantToSku = {
+         PW: ['F100-PW-220', 'F100-PW-229', 'F119', 'F135-PW-100', 'F135-PW-600'],
+   Shenyang: ['WS-10'],
+    Guizhou: ['WS-13'],
+   'Xi\'an': ['WS-15'],
+  NPOSaturn: ['AL-31F', 'AL-41F'],
+         GE: ['F404', 'F110', 'GEnx-1B64'],
+    EuroJet: ['EJ200'],
+     Snecma: ['M88'],
+         RR: ['Trent 900', 'Trent 1000'],
+}
