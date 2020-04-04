8bb10a42ef4120574391239fb265cb4c198801ba
alkashi <hoyeungw@outlook.com>
2020-04-04 17:54:07 +0800

    add enum-prompt-types

--- packages/table-gulp/src/TableLookup.js
+++ packages/table-gulp/src/TableLookup.js
@@ -5,6 +5,8 @@ import { Verse } from '@spare/verse'
 import { snakeToPascal } from '@spare/phrasing'
 import { Rename } from '@vect/rename'
 import { says } from '@palett/says'
+import { Table } from '@analys/table'
+import { IMMUTABLE } from '@analys/enum-mutabilities'
 
 /**
  *
@@ -13,6 +15,7 @@ import { says } from '@palett/says'
  * @param {string} options.key
  * @param {string} options.field
  * @param {Object} [options.config] - config for Verse.entries
+ * @param {Object} [options.filter] - config for table.find
  * @param {string} [options.dest]
  * @param {string} [options.filename]
  * @return {Function}
@@ -22,18 +25,18 @@ export const TableLookup = (options) =>
     |> Rename(says.roster(options.key) + ' -> ' + says.roster(options.field))
 
 export const tableLookup = function () {
-  /** @type {Table} */  const table = this.table
+  /** @type {Table} */  let table = this.table
   /** @type {string} */ const key = this.key
   /** @type {string} */ const field = this.field
   /** @type {string} */ const dest = this.dest
   /** @type {Object} */ const config = this.config || {}
   /** @type {string} */ const filename = this.filename || snakeToPascal(`${key}-to-${field}`)
-  const { objectify } = config
 
-  const lookups = table.lookupTable(key, field, objectify)
+  if (filter) table = Table.from(table).find(filter, IMMUTABLE)
+  const lookups = table.lookupTable(key, field, config?.objectify)
   const vinylBuffer = Vinylize(filename + '.js')
     .p(esvar(filename))
-    .p((objectify ? Verse.object : Verse.entries)(lookups, config))
+    .p((config?.objectify ? Verse.object : Verse.entries)(lookups, config))
   return dest
     ? vinylBuffer.pipe(gulp.dest(dest))
     : vinylBuffer.rest()
