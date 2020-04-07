64378c4eab034e5b0c7ac6047d0ef3e5ae0903ef
alkashi <hoyeungw@outlook.com>
2020-04-07 18:57:56 +0800

    update doc

--- packages/table-gulp/src/TableLookup.js
+++ packages/table-gulp/src/TableLookup.js
@@ -11,7 +11,7 @@ import { IMMUTABLE } from '@analys/enum-mutabilities'
 /**
  *
  * @param {Object} options
- * @param {Table}  options.table - if set, options.target and options.prop will be omitted.
+ * @param {Table}  options.[table] - if set, options.target and options.prop will be omitted.
  * @param {Table}  options.[target]
  * @param {Table}  options.[prop]
  * @param {string} options.key
